86b1fba643f28827c554160bb5e78ed8934be99f
srnbckr <soeren.becker@logsight.ai>
2025-02-27 12:52:40 +0100

    adjust exalsius logger

--- diloco_training/utils/exalsius_logger.py
+++ diloco_training/utils/exalsius_logger.py
@@ -4,41 +4,20 @@ LOG_CONFIG = {
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {
-        "access": {
-            "()": "uvicorn.logging.AccessFormatter",
-            "fmt": '%(asctime)s %(levelprefix)s [%(client_addr)s]  "%(request_line)s" %(status_code)s',
+        "detailed": {
+            "format": "%(asctime)s [%(levelname)s] [%(name)s] %(message)s",
             "datefmt": "%Y-%m-%d %H:%M:%S",
-            "use_colors": True,
-        },
-        "default": {
-            "()": "uvicorn.logging.DefaultFormatter",
-            "fmt": "%(asctime)s %(levelprefix)s [%(name)s]  %(message)s",
-            "datefmt": "%Y-%m-%d %H:%M:%S",
-            "use_colors": True,
         },
     },
     "handlers": {
-        "access": {
+        "console": {
             "class": "logging.StreamHandler",
-            "formatter": "access",
+            "formatter": "detailed",
             "stream": "ext://sys.stdout",
-        },
-        "default": {
-            "formatter": "default",
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stderr",
         },
     },
     "loggers": {
-        "exalsius": {"handlers": ["default"], "level": "DEBUG", "propagate": False},
-        "fastapi": {"handlers": ["default"], "level": "DEBUG", "propagate": False},
-        "uvicorn": {"handlers": ["default"], "level": "DEBUG", "propagate": True},
-        "uvicorn.access": {
-            "handlers": ["access"],
-            "level": "INFO",
-            "propagate": False,
-        },
-        "uvicorn.error": {"handlers": ["default"], "level": "INFO", "propagate": False},
+        "exalsius": {"handlers": ["console"], "level": "DEBUG", "propagate": False},
     },
 }
 
